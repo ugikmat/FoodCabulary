1465882684e933ed37bdfcf75cf2b7a653b1ae5c
ugikmat <ugik.mats@gmail.com>
2018-05-08 23:22:39 +0700

    Initial Commit

---
+++ app/src/main/java/id/filkom/mat/foodcab/HomeActivity.kt
@@ -0,0 +1,35 @@
+package id.filkom.mat.foodcab
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btn_sign_out.setOnClickListener {
+            mAuth!!.signOut()
+            startActivity(Intent(this@HomeActivity,LoginActivity::class.java))
+            finish()
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth?.getCurrentUser()
+
+        text_uid.text = currentUser!!.uid
+        text_name.text = currentUser?.displayName
+        text_email.text = currentUser!!.email
+    }
+}
